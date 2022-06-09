2f726ce9d9b1511e89ad21aed3a5330beea90876
nonsonoalessio <35069388+nonsonoalessio@users.noreply.github.com>
2022-06-09 19:30:12 +0200

    Creazione branch e ottimizzazione repo.

--- Linguaggio C/Alessio/Simulazioni/Esercizio3/a.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    char a = '\0', z = 'E', A = 'I', Z = 'O', ab = 'U';
-    printf("%d\n", a);
-    return 0;
-}
--- Linguaggio C/File/esercizio2/genFile.py
+++
@@ -1,8 +0,0 @@
-import string    
-import random
-from random import randint as seed  
-S = seed(0, 999)  
-ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = S))
-f = open("ciao.txt", 'w')
-f.write(str(ran))    
-f.close()
